7cb6f0c1bc261fd63ae924d128bb42fb08ef5b09
Wasachon Chaisirirat <chaisirirat.w@kaist.ac.kr>
2018-11-08 22:15:25 +0900

    add new helper function

---
+++ src/helper/helper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const createDisplayName = (user, lnameFull = true) => {
+  const fname = user.fname? user.fname: "";
+  const mname = user.mname? " " + user.mname.charAt(0) + ".": "";
+  const lname = user.lname? " " + (lnameFull?  user.lname: user.lname.charAt(0) + "."): "";
+  var displayName = "";
+
+  if (!(user.fname || user.mname || user.lname)){
+    displayName = user.username;
+  } else {
+    displayName = fname + mname + lname;
+  }
+
+  return(displayName);
+}
