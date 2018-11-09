071eb5e6f010caaddf806cecbe66df1d6b1c2f97
BUNVERDENZ <bunverdenz@gmail.com>
2018-11-10 03:03:33 +0900

    App.js
    start with Login Page instead of myboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Myboard from './myboard.js'
+// import Myboard from './myboard.js'
+import Login from './elements/login.js';
+import { Route } from 'react-router-dom'
 import './App.css';
+import ModalSwitch from './modal-switch'
 
 class App extends Component {
   render() {
     return (
-      <Myboard />
+      <Login>
+        <Route component={ModalSwitch} />
+      </Login>
     );
   }
 }
