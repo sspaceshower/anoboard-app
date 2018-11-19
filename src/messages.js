0b02c99cdcef459791d792a81414047a9216cb03
prethrolic <re.prestige@windowslive.com>
2018-11-19 19:57:52 +0900

    rename files

--- src/messages.js
+++ src/messages.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Messages extends React.Component {
   render(){
     return(
-      {}
+        <div>Messages</div>
     );
   }
 }
--- src/notification.js
+++ src/notification.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Notification extends React.Component {
   render(){
     return(
-      {}
+        <div>Notification</div>
     );
   }
 }
