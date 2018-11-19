3c375ad1c4c8134bc5c5310a069c7eee87961d2b
prethrolic <re.prestige@windowslive.com>
2018-11-19 22:12:20 +0900

    add grouppage.js

---
+++ src/Grouppage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Group from './components/group.js'
+
+function Grouppage({ match }){
+  const groupname = match.params.groupname;
+  const group = {};
+  //TODO: <backend> use username to fectch board object which board.
+  return(
+    <div>
+    </div>
+  )
+}
