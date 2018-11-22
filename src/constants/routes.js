4cc77292f90a65bc808860eda20a22e6b044d04b
prethrolic <re.prestige@windowslive.com>
2018-11-22 23:38:44 +0900

    signout workaround

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,7 @@
 export const LANDING = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
+export const SIGN_OUT = '/signout';
 export const PASSWORD_FORGET = '/pw-forget';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
